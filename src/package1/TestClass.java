9c49db11c3a0740570dbbd7e9af23267ffb81313
VadimDem <vagik51@gmail.com>
2024-01-31 10:43:24 +0300

    Изменения в TestClass

--- src/package1/TestClass.java
+++ src/package1/TestClass.java
@@ -1,4 +1,10 @@
 package package1;
 
 public class TestClass {
+    public static void TestClass(String[] args) {
+        System.out.println("sout");
+        System.out.printf("souf");
+
+    }
 }
+
