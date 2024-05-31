b0253cf26c45fb5760dcc6661a5e757698f09c51
VadimDem <vagik51@gmail.com>
2024-05-31 16:53:47 +0300

    commit2

--- src/main/java/ru/lwjgl/Group.java
+++ src/main/java/ru/lwjgl/Group.java
@@ -1,20 +1,26 @@
 package ru.lwjgl;
 
 public class Group {
-    private int groupNum;
-    Student[] students = new Student[20];
-    int studentCount = 0;
+    private int numberGroup;
+    private Student[] students = new Student[20];
+    private int studentCount = 0;
 
     public Group(int number) {
-        this.groupNum = number;
-        this.students = new Student[20];
-        this.studentCount = 0;
+        this.numberGroup = number;
     }
 
-    public void addStudent(){
-        if (studentCount < 20){
+    public void addStudent(Student student) {
+        if (studentCount < 20) {
             students[studentCount] = student;
-
+            studentCount++;
+        } else {
+            System.out.println("Груупа Заполнена");
+        }
+    }
+    public void printStudents(){
+        System.out.println("Студенты в группе " + numberGroup + ":");
+        for (int i = 0; i < studentCount; i++) {
+            System.out.println(students[i].toString());
         }
     }
 }
